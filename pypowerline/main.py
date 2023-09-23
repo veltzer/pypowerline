1f465d4139de1cdcad2f86ae39d0d6c3fd2fbf17
Mark Veltzer <mark.veltzer@gmail.com>
2023-09-23 13:31:44 +0300

    unifying

--- pypowerline/main.py
+++ pypowerline/main.py
@@ -4,7 +4,31 @@ main
 import os
 from pytconf import register_endpoint, register_main, config_arg_parse_and_launch
 
+from termcolor import cprint
+
 from pypowerline.static import DESCRIPTION, APP_NAME, VERSION_STR
+
+
+symbols = {
+    'detached': '\u2693',
+    'ahead': '\u2B06',
+    'behind': '\u2B07',
+    'staged': '\u2714',
+    'changed': '\u270E',
+    'new': '?',
+    'conflicted': '\u273C',
+    'stash': '\u2398',
+    'git': '\uE0A0',
+    'hg': '\u263F',
+    'bzr': '\u2B61\u20DF',
+    'fossil': '\u2332',
+    'svn': '\u2446',
+    'separator': '\u25B6',
+    'separator_b': '\uE0B0',
+    'separator_thin': '\u276F',
+    'separator_thin_b': '\uE0B1',
+    'lock': '\uE0A2',
+}
 
 
 @register_endpoint(
@@ -13,7 +37,9 @@ from pypowerline.static import DESCRIPTION, APP_NAME, VERSION_STR
 )
 def bash() -> None:
     cwd = os.getcwd()
-    print(f"{cwd} > ", end="")
+    # print(f"{cwd} > ", end="")
+    cprint(f"{cwd}", "black", "on_light_magenta", end="")
+    cprint(f"{symbols['separator_b']}", "light_cyan", "on_black", end="")
 
 
 @register_endpoint(
@@ -23,6 +49,7 @@ def bash() -> None:
 def tmux() -> None:
     cwd = os.getcwd()
     print(f"{cwd} > ", end="")
+    cprint(f"{cwd}", "red", "on_light_magenta", end="")
 
 
 @register_endpoint(
@@ -30,26 +57,6 @@ def tmux() -> None:
     description="print special symbols (used for development)",
 )
 def dump_symbols() -> None:
-    symbols = {
-        'detached': '\u2693',
-        'ahead': '\u2B06',
-        'behind': '\u2B07',
-        'staged': '\u2714',
-        'changed': '\u270E',
-        'new': '?',
-        'conflicted': '\u273C',
-        'stash': '\u2398',
-        'git': '\uE0A0',
-        'hg': '\u263F',
-        'bzr': '\u2B61\u20DF',
-        'fossil': '\u2332',
-        'svn': '\u2446',
-        'separator': '\u25B6',
-        'separator_b': '\uE0B0',
-        'separator_thin': '\u276F',
-        'separator_thin_b': '\uE0B1',
-        'lock': '\uE0A2',
-    }
     for k, v in symbols.items():
         print(k, v)
 
