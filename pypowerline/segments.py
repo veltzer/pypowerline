766ef49e0e5697eca7aa085d70810ca230b29731
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-02 15:09:25 +0300

    new version

--- pypowerline/segments.py
+++ pypowerline/segments.py
@@ -3,33 +3,42 @@ from abc import ABC, abstractmethod
 import os
 import os.path
 
+from pypowerline.symbols import Symbol
+
 
 class Segment(ABC):
-    def __init__(self):
-        pass
-
-    def setup(self):
-        pass
+    def __init__(self, show_icon: bool = True):
+        self.show_icon = show_icon
 
     @abstractmethod
-    def get_text(self):
+    def get_text(self) -> str:
         """ return the text of the segment """
-    def get_color(self):
-        """ return the color of the segment """
-    def get_background_color(self):
-        """ return the background color of the segment """
-    def get_icon(self):
+
+    def get_icon(self) -> Symbol:
         """ return the special character of the segment """
+        return Symbol.NONE
+
+    # def get_color(self) -> str:
+    #     """ return the color of the segment """
+    # def get_background_color(self) -> str:
+    #     """ return the background color of the segment """
 
 
 class SegmentCwd(Segment):
-    def __init__(self, color="green", last_only=False, home_as_tilde=True):
+    def __init__(
+            self,
+            color: str = "green",
+            last_only: bool = False,
+            home_as_tilde: bool = True,
+            show_icon: bool = True,
+    ):
+        super().__init__(show_icon=show_icon)
         self.color = color
         self.last_only = last_only
         self.home_as_tilde = home_as_tilde
         self.home_directory = os.path.expanduser("~")
 
-    def get_text(self):
+    def get_text(self) -> str:
         cwd = os.getcwd()
         if self.home_as_tilde and cwd.startswith(self.home_directory):
             cwd = "~" + cwd[len(self.home_directory):]
@@ -37,7 +46,13 @@ class SegmentCwd(Segment):
             return os.path.basename(cwd)
         return cwd
 
+    def get_icon(self) -> Symbol:
+        return Symbol.FOLDER
+
 
 class SegmentForward(Segment):
+    def __init__(self):
+        super().__init__(show_icon=False)
+
     def get_text(self):
         return " > "
--- pypowerline/static.py
+++ pypowerline/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.17"
+VERSION_STR = "0.0.18"
 DESCRIPTION = "sane powerline"
 APP_NAME = "pypowerline"
 LOGGER_NAME = "pypowerline"
