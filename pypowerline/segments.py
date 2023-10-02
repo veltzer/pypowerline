f457bd5b2bdd7f4b079d70ad1797f7bd40ebd44e
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-02 11:05:35 +0300

    new version

--- pypowerline/segments.py
+++ pypowerline/segments.py
@@ -8,11 +8,15 @@ class Segment(ABC):
     def __init__(self):
         pass
 
+    def setup(self):
+        pass
+
     @abstractmethod
     def get_text(self):
         """ return the text of the segment """
     def get_color(self):
         """ return the color of the segment """
+        return self.color
     def get_background_color(self):
         """ return the background color of the segment """
     def get_icon(self):
@@ -20,8 +24,8 @@ class Segment(ABC):
 
 
 class SegmentCwd(Segment):
-    def __init__(self):
-        self.color = None
+    def setup(self):
+        self.color = "green"
         self.last_only = True
         self.home_as_tilde = True
         self.home_directory = os.path.expanduser("~")
@@ -33,16 +37,6 @@ class SegmentCwd(Segment):
         if self.last_only:
             return os.path.basename(cwd)
         return cwd()
-
-    def get_color(self):
-        return self.color
-
-    def get_background_color(self):
-        return "black"
-
-    def get_icon(self):
-        return None
-
 
 class SegmentForward(Segment):
     def get_text(self):
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pypowerline",
-    version="0.0.10",
+    version="0.0.11",
     packages=[
         "pypowerline",
     ],
