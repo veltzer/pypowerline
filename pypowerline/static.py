c72545cc5fbefc5d6152697bd65462fb877d177b
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-02 17:10:00 +0300

    new version

--- pypowerline/static.py
+++ pypowerline/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.25"
+VERSION_STR = "0.0.26"
 DESCRIPTION = "sane powerline"
 APP_NAME = "pypowerline"
 LOGGER_NAME = "pypowerline"
