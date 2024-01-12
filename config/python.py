db64d448d5adfed13b9299b6bd1396704858ff71
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-12 15:07:24 +0200

    more python.py changes

--- config/python.py
+++ config/python.py
@@ -13,7 +13,7 @@ config_requires: List[str] = [
 install_requires: List[str] = [
     "pytconf",
 ]
-make_requires: List[str] = [
+build_requires: List[str] = [
     "pymakehelper",
     "pydmt",
 ]
@@ -24,4 +24,4 @@ test_requires: List[str] = [
     "flake8",
     "mypy",
 ]
-requires = config_requires + install_requires + make_requires + test_requires
+requires = config_requires + install_requires + build_requires + test_requires
