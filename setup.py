7601bb21e2db5ee0017ad6a40349c67b05d3472e
Mark Veltzer <mark.veltzer@gmail.com>
2024-10-12 15:07:17 +0300

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pypowerline",
-    version="0.0.31",
+    version="0.0.32",
     packages=[
         "pypowerline",
     ],
