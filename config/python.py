f7b45b500c1cdfd6c3bdd1431c4740837b7e2046
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:16 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -1,25 +1,14 @@
 """ python deps for this project """
 
-scripts: dict[str,str] = {
-    "pypowerline": "pypowerline.main:main",
-}
+import config.shared
 
-config_requires: list[str] = [
-    "pyclassifiers",
-]
 install_requires: list[str] = [
     "pytconf",
 ]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-]
-test_requires: list[str] = [
-    "pytest",
-    "pylint",
-    "mypy",
-    "ruff",
-]
-requires = config_requires + install_requires + build_requires + test_requires
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
+requires = install_requires + build_requires + test_requires
+
+scripts: dict[str,str] = {
+    "pypowerline": "pypowerline.main:main",
+}
